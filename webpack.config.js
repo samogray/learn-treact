a26bf2dff6a453aa8f841323e8b4a3fc621578fb
Samogray Olexandr <sam_gray@ukr.net>
2016-11-15 00:31:29 +0200

    style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,25 @@ module.exports = {
         filename: "./dist/bundle.js"
     },
     module: {
-        loaders: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            query: {
-                presets: ['es2015'],
-                cacheDirectory: true
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015'],
+                    cacheDirectory: true
+                }
+            },
+            {
+                test: /\.css$/, loader: "style!css"
+            },
+            {
+                test: /\.scss$/,
+                loaders: ["style", "css?sourceMap", "sass?sourceMap"]
             }
-        }],
+        ],
         devtool: "source-map"
-    }
+    },
+    watch: true
 }
