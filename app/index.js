d5f310ca15788bbae695937dda70f2756d0d2961
Samogray Olexandr <sam_gray@ukr.net>
2016-11-17 00:55:32 +0200

    add bootstrap

--- app/index.js
+++ app/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TopMenu from './nav-menu/index.js'
+import { Grid, Row, Col } from 'react-bootstrap';
+
 import './style/style.scss';
 
 ReactDOM.render(
-  <h1>Alex</h1>,
+  <Grid fluid = {true}>
+      <Row>
+          <Col xs={12}>
+              <TopMenu/>
+          </Col>
+      </Row>
+  </Grid>,
   document.getElementById('root')
 );
