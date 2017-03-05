24a4375c0b1c004c5b4d20cdef5480abe566021b
Samogray Olexandr <oleksandrs@weblium.com>
2017-03-05 21:41:44 +0200

    migrate to webpack2

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = () => ({
+  plugins: {
+    'autoprefixer': {browsers: ['last 2 versions']},
+    'postcss-inline-svg': {},
+    'postcss-svgo': {}
+  }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -6,62 +6,69 @@ const path = require('path');
 const autoprefixer = require('autoprefixer');
 const postcss = require('postcss');
 const cssnano = require('cssnano');
+const localIdentName = '[local]_[hash:base64:3]'
 
 module.exports = {
-    entry: {
-        main: ['webpack-dev-server/client','webpack/hot/dev-server','./app']
-    },
-    output: {
-        path: `${__dirname}/dist`,
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    presets: ["es2015","react"],
-                    cacheDirectory: true
-                }
-            },
-            {
-                test: /\.css$/, loader: "style!css"
-            },
-            {
-                test: /\.scss$/,
-                loaders: [
-                    'style',
-                    'css?sourceMap&modules&importLoaders=1&localIdentName=[local]_[hash:base64:3]',
-                    'postcss?sourceMap',
-                    'resolve-url',
-                    'sass?sourceMap'
-                ]
-            },
-            {
-                test: /\.(jpe?g|png|gif)$/i,
-                loader: 'file?name=images/[name].[ext]?[hash]'
-            }, {
-                test: /\.(svg)/,
-                loader: 'file?name=images/[name].[ext]?[hash]'
-            }, {
-                test: /\.font$/,
-                loader: 'style!css!fontgen?fileName=fonts/[fontname].[ext]?[hash]'
-            }, {
-                test: /\.(eot|ttf|woff|woff2)/,
-                loader: 'file?name=fonts/[name].[ext]?[hash]'
+  entry: './app',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ["es2015", "react"],
+          cacheDirectory: true
+        }
+      }, {
+        test: /\.css$/, loader: "style!css"
+      }, {
+        test: /\.scss$/,
+        use: [
+          {loader: 'style-loader'},
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              sourceMap: true,
+              localIdentName
             }
-        ]
-    },
-    postcss: function postcss() {
-        return [autoprefixer({browsers: ['last 2 versions']}), cssnano()]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    devServer: {
-        contentBase:'./dist',
-        hot: true
-    }
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            }
+          }, {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            }
+          }]
+      }, {
+        test: /\.(jpe?g|png|gif)$/i,
+        loader: 'file?name=images/[name].[ext]?[hash]'
+      }, {
+        test: /\.(svg)/,
+        loader: 'file?name=images/[name].[ext]?[hash]'
+      }, {
+        test: /\.font$/,
+        loader: 'style!css!fontgen?fileName=fonts/[fontname].[ext]?[hash]'
+      }, {
+        test: /\.(eot|ttf|woff|woff2)/,
+        loader: 'file?name=fonts/[name].[ext]?[hash]'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
 }
